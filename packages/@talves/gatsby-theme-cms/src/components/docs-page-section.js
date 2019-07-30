a3d701359883f3ce978a8ef6a37aef6f13a64bfc
talves <tony@3alves.com>
2019-07-30 13:16:15 -0700

    fix: render some compiled mdx

--- packages/@talves/gatsby-theme-cms/src/components/docs-page-section.js
+++ packages/@talves/gatsby-theme-cms/src/components/docs-page-section.js
@@ -1,15 +1,16 @@
 import React from "react";
-import Markdown from "react-markdown";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import { Link } from "gatsby";
 
 const DocsPageSection = (data) => {
+console.log(data.parent.body)
   return (
     <>
-    <h1>{data.frontmatter && data.frontmatter.title}</h1>
+      <h1>{data.frontmatter && data.frontmatter.title}</h1>
       <h2>
         <Link to={`/${data.internal.type.toLowerCase()}`}>back</Link>
       </h2>
-      <Markdown>{data.rawBody}</Markdown>
+      <MDXRenderer>{data.parent.body}</MDXRenderer>
     </>
   )
 }
